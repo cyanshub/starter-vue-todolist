53227c11e22d3d9f6f61767995d04ec1fa5a23ba
travel15740 <travel15740@gmail.com>
2025-06-29 00:05:05 +0800

    init: 知道在哪導入 router 和 vuex

--- src/main.js
+++ src/main.js
@@ -5,7 +5,10 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+// 在 Vue 2 中，使用 `Vue.use()` 来安装插件
 new Vue({
+  // 在 Vue 2 中，使用 `render` 函数来渲染根组件
+  // 導入 store 和 router, 使用 Vuex 和 Vue Router
   router,
   store,
   render: h => h(App)
